f88d8c46d9d6add490cf326edb6ab34b30a42035
Robbie <53794108+foogolplex@users.noreply.github.com>
2022-06-25 03:55:35 -0500

    Update update.py

--- update.py
+++ update.py
@@ -7,29 +7,47 @@ action = os.getenv("action")
 
 stats = {}
 data = {}
-with open('players.json') as players:
+with open("players.json") as players:
     data = json.load(players)
     players.close()
+with open("stats.json") as sdata:
+    stats = json.load(sdata)
+    sdata.close()
 
 def act():
-  if action == "feed":
-    stats["totalfed"] += 1
-    stats["currentfed"] += 1
+    if action == "feed":
+        stats["totalfed"] += 1
+        stats["currentfed"] += 1
     if user not in data["angels"]:
-      data["angels"][user] = 1
+        data["angels"][user] = 1
     else:
-      data["angels"][user] += 1
-  elif action == "kill":
-    stats["deathtoll"] += 1
-    data["murderers"].append(user)
-    generateNew()
+        data["angels"][user] += 1
+    elif action == "kill":
+        stats["deathtoll"] += 1
+        data["murderers"].append(user)
+        generateNew()
 
-firstNames = []
-lastNames = []
+firstNames = ["Abraham", "Muhumammad ibn", "Alan", "Albert", "Albertus", "Alexander", "Alfred North", "Andrew", "Archimedes", "Aristotle", "Arthur", "Augustin-Louis", "Augustus", "Benjamin", "Bernhard", "Bertrand", "Blaise", "Brook", "Carl Friedrich", "Charles", "David", "Diophantus"]
+lastNames = ["De Moivre", "Khwarizmi", "Turing", "Einstein", "Magnus", "Grothendieck", "Whitehead", "Wiles", "Cayley", "Cauchy", "De Morgan", "Banneker", "Riemann", "Russel", "Pascal", "Taylor", "Gauss", "Babbage", "Bernoulli", "Hilbert"]
 def generateNew():
-  stats["currentfed"] = 0
-  stats["name"] = firstNames[randint(0,len(firstNames))] + lastNames[randint(0,len(lastNames))]
-  
+    stats["currentfed"] = 0
+    stats["name"] = firstNames[randint(0,len(firstNames))] + lastNames[randint(0,len(lastNames))]
+
+def updateReadme():
+    with open("README.md", "w") as f:
+        f.write("""
+            
+        """
+        f.close()
 # if not a murderer then act
 if user not in data["murderers"]:
-  act()
+    act()
+    updateReadme()
+
+    # save changes
+    with open("players.json", "w") as p:
+        json.dumps(data, p)
+        p.close()
+    with open("stats.json", "w") as s:
+        json.dumps(stats, s)
+        s.close()
