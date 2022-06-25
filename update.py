4a21812d63c02db08fb2eca5ebb036f8577d44ef
Robbie <53794108+foogolplex@users.noreply.github.com>
2022-06-25 03:27:46 -0500

    Update update.py

--- update.py
+++ update.py
@@ -1,15 +1,35 @@
 import os
 import json
+import randint from random
 
 user = os.getenv("user")
 action = os.getenv("action")
 
-def main():
-  if action == "feed":
-    pass
-  elif action == "kill":
-    pass
-  elif action == "gains":
-    pass
+stats = {}
+data = {}
+with open('players.json') as players:
+    data = json.load(players)
+    players.close()
 
-main()
+def act():
+  if action == "feed":
+    stats["totalfed"] += 1
+    stats["currentfed"] += 1
+    if user not in data["angels"]:
+      data["angels"][user] = 1
+    else:
+      data["angels"][user] += 1
+  elif action == "kill":
+    stats["deathtoll"] += 1
+    data["murderers"].append(user)
+    generateNew()
+
+firstNames = []
+lastNames = []
+def generateNew():
+  stats["currentfed"] = 0
+  stats["name"] = firstNames[randint(0,len(firstNames))] + lastNames[randint(0,len(lastNames))]
+  
+# if not a murderer then act
+if user not in data["murderers"]:
+  act()
