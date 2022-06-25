d375eb3474379cd718df11168eb7e7991586d5da
Robbie <53794108+foogolplex@users.noreply.github.com>
2022-06-25 16:07:12 -0500

    Update update.py

--- update.py
+++ update.py
@@ -38,9 +38,29 @@ def generateNew():
 
 def updateReadme():
     with open("README.md", "w") as f:
-        f.write("""<p align="center">##Hi, I'm Rob</p>.\n[feed](https://github.com/foogolplex/foogolplex/issues/new?title=feed&body=just+click+submit+and+feed+they+will)\n
-[kill](https://github.com/foogolplex/foogolplex/issues/new?title=kill&body=just+click+submit+and+they+will+die+but+be+warned+that+you+will+be+revoked+from+your+privileges)
-""")
+        f.write('''<p align="center">Hi, I'm Rob.</p>
+
+<p align="center"><i>Insert a meta, humble, "individualizing" hogwash of a blarney you've seen a septillion times to convince you I'm normal, competent and unique.</i></p>
+
+<br>
+<br>
+
+<h1 align="center">
+Dilemma
+</h1>
+
+<p align="center">
+<a href=https://github.com/foogolplex/foogolplex/issues/new?title=feed&body=just+click+submit+and+feed+they+will>feed</a>
+</p>
+<p align="center">
+<a href=https://github.com/foogolplex/foogolplex/issues/new?title=kill&body=just+click+submit+and+they+will+die+but+be+warned+that+you+will+be+revoked+from+your+privileges>kill</a>
+</p>
+
+''')
+        f.write("Name: " + stats["name"])
+        f.write("Streak (amount currently fed): " + str(stats["currentfed"]))
+        f.write("Total times fed: " + stats["totalfed"])
+        f.write("Deathtoll: " + stats["deathtoll"])
         f.close()
 # if not a murderer then act
 if user not in data["murderers"]:
