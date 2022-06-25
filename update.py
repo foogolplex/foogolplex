046806c902c9ed401f49515dc95048af20e6d063
Robbie <53794108+foogolplex@users.noreply.github.com>
2022-06-25 03:01:55 -0500

    init

---
+++ update.py
@@ -0,0 +1,15 @@
+import os
+import json
+
+user = os.getenv("user")
+action = os.getenv("action")
+
+def main():
+  if action == "feed":
+    pass
+  elif action == "kill":
+    pass
+  elif action == "gains":
+    pass
+
+main()
